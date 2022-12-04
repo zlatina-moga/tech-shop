a691937b7639f06fe68821b313232e334b38b64d
zlatina-moga <zlatina.delcheva@gmail.com>
2022-12-04 09:45:03 +0200

    login and registration added

---
+++ pages/login.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+
+const Login = () => {
+  return (
+    <div className="col-md-4 col-md-offset-4 mt-10" id="login">
+      <section id="inner-wrapper" className="login">
+        <article>
+          <form>
+            <div className="form-group">
+              <div className="input-group">
+                <span className="input-group-addon">
+                  <i className="fa fa-user"> </i>
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="input-group">
+                <span className="input-group-addon">
+                  <i className="fa fa-envelope"> </i>
+                </span>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email Address"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="input-group">
+                <span className="input-group-addon">
+                  <i className="fa fa-key"> </i>
+                </span>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="input-group">
+                <span className="input-group-addon">
+                  <i className="fa fa-key"> </i>
+                </span>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Confirm Password"
+                />
+              </div>
+            </div>
+            <button type="submit" className="btn btn-success btn-block">
+              Submit
+            </button>
+          </form>
+        </article>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
