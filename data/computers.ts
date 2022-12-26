686e6c7d5dba8c0701236f2ea3a42373405364c3
zlatina-moga <zlatina.delcheva@gmail.com>
2022-12-26 10:23:21 +0200

    added computers data

--- data/computers.ts
+++ data/computers.ts
@@ -1,7 +1,9 @@
-import computerLenovoThinkCentreImg1 from '../public/products/computers/Calculator-Lenovo-ThinkCentre-M82-Tower-Intel-Core-i3-2100-3-10-GHz-4GB-DDR3-250GB-HDD-DVD-Windows-1_2.webp';
-import computerLenovoThinkCentreImg2 from '../public/products/computers/Calculator-Lenovo-ThinkCentre-M82-Tower-Intel-Core-i3-2100-3-10-GHz-4GB-DDR3-250GB-HDD-DVD-Windows-1.webp';
-import computerLenovoThinkCentreImg3 from '../public/products/computers/Calculator-Lenovo-ThinkCentre-M82-Tower-Intel-Core-i3-2100-3-10-GHz-4GB-DDR3-250GB-HDD-DVD-Windows-1(1).webp';
-import computerLenovoThinkCentreImg4 from '../public/products/computers/Calculator-Lenovo-ThinkCentre-M82-Tower-Intel-Core-i3-2100-3-10-GHz-4GB-DDR3-250GB-HDD-DVD-Windows-1_1.webp';
+import computerLenovoThinkCentreImg1 from "../public/products/computers/Calculator-Lenovo-ThinkCentre-M82-Tower-Intel-Core-i3-2100-3-10-GHz-4GB-DDR3-250GB-HDD-DVD-Windows-1_2.webp";
+import computerLenovoThinkCentreImg2 from "../public/products/computers/Calculator-Lenovo-ThinkCentre-M82-Tower-Intel-Core-i3-2100-3-10-GHz-4GB-DDR3-250GB-HDD-DVD-Windows-1.webp";
+import computerLenovoThinkCentreImg3 from "../public/products/computers/Calculator-Lenovo-ThinkCentre-M82-Tower-Intel-Core-i3-2100-3-10-GHz-4GB-DDR3-250GB-HDD-DVD-Windows-1(1).webp";
+import computerLenovoThinkCentreImg4 from "../public/products/computers/Calculator-Lenovo-ThinkCentre-M82-Tower-Intel-Core-i3-2100-3-10-GHz-4GB-DDR3-250GB-HDD-DVD-Windows-1_1.webp";
+import computerFujitsiImg2 from '../public/products/computers/Calculator-Fujitsu-Esprimo-Q920-Mini--Intel-Core-i3-4130-3-40-GHz--4GB-DDR3--120-SSD-Refurbished.jpg';
+import computerFujitsiImg1 from '../public/products/computers/Calculator-Fujitsu-Esprimo-Q920-Mini--Intel-Core-i3-4130-3-40-GHz--4GB-DDR3--120-SSD-Refurbished.png';
 
 export interface IComputer {
   category?: string;
@@ -102,70 +104,153 @@ export interface IComputer {
 
 export const computersData: IComputer[] = [
   {
-    id: 'calculator-lenovo-thinkcentre-m82-tower-intel-core-i3-2100-3.10-ghz-4gb-ddr3-250gb-hdd-dvd-windows-10-pro-refurbished-preinstalat',
-    images: [computerLenovoThinkCentreImg1, computerLenovoThinkCentreImg2, computerLenovoThinkCentreImg3, computerLenovoThinkCentreImg4],
-    category: 'calculatoare-refurbished',
-    title: 'Calculator Lenovo ThinkCentre M82 Tower, Intel Core i3-2100 3.10 GHz, 4GB DDR3, 250GB HDD, DVD, Windows 10 Pro Refurbished Preinstalat',
+    id: "calculator-lenovo-thinkcentre-m82-tower-intel-core-i3-2100-3.10-ghz-4gb-ddr3-250gb-hdd-dvd-windows-10-pro-refurbished-preinstalat",
+    images: [
+      computerLenovoThinkCentreImg1,
+      computerLenovoThinkCentreImg2,
+      computerLenovoThinkCentreImg3,
+      computerLenovoThinkCentreImg4,
+    ],
+    category: "calculatoare-refurbished",
+    title:
+      "Calculator Lenovo ThinkCentre M82 Tower, Intel Core i3-2100 3.10 GHz, 4GB DDR3, 250GB HDD, DVD, Windows 10 Pro Refurbished Preinstalat",
     price: 381.36,
-    oldPrice: 476.70,
+    oldPrice: 476.7,
     discount: 95.34,
     inStock: true,
-    currency: 'lei',
-    details: 'Toate fotografiile produselor prezentate au caracter informativ, pot diferi fata de produsul vandut si pot arata accesorii ce nu sunt incluse in pachetul standard al produsului',
-    internalCode: 'LNTCM82TWRRE01P',
-    model: 'ThinkCentre M82',
+    currency: "lei",
+    details:
+      "Toate fotografiile produselor prezentate au caracter informativ, pot diferi fata de produsul vandut si pot arata accesorii ce nu sunt incluse in pachetul standard al produsului",
+    internalCode: "LNTCM82TWRRE01P",
+    model: "ThinkCentre M82",
     producerDetails: {
-      name: 'Lenovo',
-      model: 'ThinkCentre M82',
-      warranty: '12 luni'
+      name: "Lenovo",
+      model: "ThinkCentre M82",
+      warranty: "12 luni",
     },
     processorDetails: {
-      processorModel: 'i3-2100',
+      processorModel: "i3-2100",
       nuclearNumber: 2,
-      processorType: 'Intel Core i3',
-      cpuCache: '3',
-      frequencyCPU: '3.10',
-      graphicProcessor: 'Intel® HD Graphics 2000',
-      socket: 'LGA 1155'
+      processorType: "Intel Core i3",
+      cpuCache: "3",
+      frequencyCPU: "3.10",
+      graphicProcessor: "Intel® HD Graphics 2000",
+      socket: "LGA 1155",
     },
     memoryCapacity: 4,
-    memoryType: 'DDR3',
+    memoryType: "DDR3",
     hardDiskDetails: {
-      capacity: '250 GB',
-      format: '3.5 inch',
-      interface: 'SATA-III',
+      capacity: "250 GB",
+      format: "3.5 inch",
+      interface: "SATA-III",
       rotation: 7200,
-      type: 'HDD'
+      type: "HDD",
     },
     motherBoard: {
       RAMcapacity: 32,
       psi_e_x8: 1,
-      chipset: 'Intel Q75',
-      cpuSocket: 'LGA 1155',
+      chipset: "Intel Q75",
+      cpuSocket: "LGA 1155",
       psi_e_x1: 1,
       psi_e_x16: 1,
       memorySlots: 4,
-      psiSlots: 1
+      psiSlots: 1,
     },
     layout: {
-      color: 'Negru',
-      dimensions: '175 x 417 x 360',
-      shape: 'Tower',
-      weight: '11'
+      color: "Negru",
+      dimensions: "175 x 417 x 360",
+      shape: "Tower",
+      weight: "11",
     },
-    operationSystem: 'Windows 10 Professional Mar',
+    operationSystem: "Windows 10 Professional Mar",
     videoCard: {
-      capacity: 'up to 2 GB',
-      chip: 'Intel',
+      capacity: "up to 2 GB",
+      chip: "Intel",
       memoryFrequency: 1333,
-      tip: 'DDR3'
+      tip: "DDR3",
     },
     powerSupply: 280,
     ports: {
       audio: 1,
       serialPort: 1,
       usb_2: 8,
-      usb_3: 6
-    }
-  }
-]
+      usb_3: 6,
+    },
+  },
+  {
+    id: "calculator-fujitsu-esprimo-q920-mini-intel-core-i3-4130-3.40-ghz-4gb-ddr3-120-ssd-refurbished",
+    images: [
+      computerFujitsiImg1,
+      computerFujitsiImg2
+    ],
+    category: "calculatoare-refurbished",
+    title:
+      "Calculator Fujitsu Esprimo Q920 Mini, Intel Core i3-4130T 2.90 GHz, 4GB DDR3, 120 SSD, Refurbished",
+    price: 367.87,
+    oldPrice: 459.84,
+    discount: 91.97,
+    inStock: true,
+    currency: "lei",
+    details:
+      "Toate fotografiile produselor prezentate au caracter informativ, pot diferi fata de produsul vandut si pot arata accesorii ce nu sunt incluse in pachetul standard al produsului",
+    internalCode: "CFQ920SFFREM01",
+    model: "Q920",
+    producerDetails: {
+      name: "Fujitsu Siemens",
+      model: "Q920",
+      warranty: "12 luni",
+    },
+    processorDetails: {
+      processorModel: "i3-4130t",
+      nuclearNumber: 2,
+      processorType: "Intel Core i3",
+      cpuCache: "3",
+      frequencyCPU: "2.90",
+      graphicProcessor: "Intel® HD Graphics 4400",
+      socket: "LGA 1150",
+    },
+    memoryCapacity: 4,
+    memoryType: "DDR3",
+    hardDiskDetails: {
+      capacity: "120 GB",
+      format: "2.5 inch",
+      interface: "SATA-III",
+      rotation: 7200,
+      type: "SSD",
+    },
+    motherBoard: {
+      RAMcapacity: 16,
+      psi_e_x8: 1,
+      chipset: "Intel Q87",
+      cpuSocket: "LGA 1150",
+      psi_e_x1: 2,
+      psi_e_x16: 1,
+      memorySlots: 2,
+      psiSlots: 1,
+      typeMemory: 'DDR3',
+      producer: 'Fujitsu'
+    },
+    layout: {
+      color: "Negru",
+      dimensions: "185 x 188 x 54 mm",
+      shape: "Mini PC",
+      weight: "1.6",
+    },
+    refurbished: true,
+    operationSystem: "Fara sistem de operare",
+    videoCard: {
+      chip: "Intel",
+      memoryFrequency: 1333,
+      tip: "HD Graphics",
+      supportDirectX: 11.1
+    },
+    powerSupply: 65,
+    ports: {
+      audio: 2,
+      serialPort: 1,
+      usb_2: 2,
+      usb_3: 4,
+      network: 1
+    },
+  },
+];
