ee504b1c04e918c8e7603d8758b72266da6b4d3e
zlatina-moga <zlatina.delcheva@gmail.com>
2022-12-29 12:11:01 +0200

    added single image view computers

--- data/computers.ts
+++ data/computers.ts
@@ -4,6 +4,11 @@ import computerLenovoThinkCentreImg3 from "../public/products/computers/Calculat
 import computerLenovoThinkCentreImg4 from "../public/products/computers/Calculator-Lenovo-ThinkCentre-M82-Tower-Intel-Core-i3-2100-3-10-GHz-4GB-DDR3-250GB-HDD-DVD-Windows-1_1.webp";
 import computerFujitsiImg2 from '../public/products/computers/Calculator-Fujitsu-Esprimo-Q920-Mini--Intel-Core-i3-4130-3-40-GHz--4GB-DDR3--120-SSD-Refurbished.jpg';
 import computerFujitsiImg1 from '../public/products/computers/Calculator-Fujitsu-Esprimo-Q920-Mini--Intel-Core-i3-4130-3-40-GHz--4GB-DDR3--120-SSD-Refurbished.png';
+import HPProdeskImg1 from '../public/products/computers/HP-Prodesk-600-G4-Tower-Intel-Core-i7-8700T-2-40-GHz--16GB-DDR4--256GB-SSD--Intel-UHD-Graphics-630---Windows-10-Pro-Mar-preinstalat.jpg';
+import HPProdeskImg2 from '../public/products/computers/HP-Prodesk-600-G4-Tower-Intel-Core-i7-8700T-2-40-GHz--16GB-DDR4--256GB-SSD--Intel-UHD-Graphics-630---Windows-10-Pro-Mar-preinstalat.png';
+import HPProdeskImg3 from '../public/products/computers/HP-Prodesk-600-G4-Tower-Intel-Core-i7-8700T-2-40-GHz--16GB-DDR4--256GB-SSD--Intel-UHD-Graphics-630---Windows-10-Pro-Mar-preinstalat_838x-n4.png';
+import HPProdeskImg4 from '../public/products/computers/HP-Prodesk-600-G4-Tower-Intel-Core-i7-8700T-2-40-GHz--16GB-DDR4--256GB-SSD--Intel-UHD-Graphics-630---Windows-10-Pro-Mar-preinstalat_97r0-gn.png';
+import HPProdeskImg5 from '../public/products/computers/HP-Prodesk-600-G4-Tower-Intel-Core-i7-8700T-2-40-GHz--16GB-DDR4--256GB-SSD--Intel-UHD-Graphics-630---Windows-10-Pro-Mar-preinstalat_bwy5-h2.png';
 
 export interface IComputer {
   category?: string;
@@ -47,9 +52,9 @@ export interface IComputer {
   };
   motherBoard: {
     typeMemory?: string;
-    RAMcapacity: number;
+    RAMcapacity?: number;
     producer?: string;
-    memorySlots: number;
+    memorySlots?: number;
     cpuSocket?: string;
     chipset?: string;
     psi_e_x1?: number;
@@ -57,7 +62,7 @@ export interface IComputer {
     psi_e_x16?: number;
     psiSlots?: number;
   };
-  layout: {
+  layout?: {
     color: string;
     dimensions: string;
     shape: string;
@@ -67,7 +72,7 @@ export interface IComputer {
   operationSystem?: string;
   secondHand?: boolean;
   refurbished?: boolean;
-  videoCard: {
+  videoCard?: {
     chip: string;
     memoryFrequency: number;
     tip: string;
@@ -78,7 +83,7 @@ export interface IComputer {
     slotType?: string;
   };
   powerSupply?: number;
-  ports: {
+  ports?: {
     audio: any;
     usb_2?: number;
     usb_3?: number;
@@ -89,6 +94,7 @@ export interface IComputer {
     hdmi?: number;
     serialPort?: number;
   };
+  hasNoOS?: boolean;
   summary?: {
     model: string;
     processor: string;
@@ -162,6 +168,7 @@ export const computersData: IComputer[] = [
       shape: "Tower",
       weight: "11",
     },
+    hasNoOS: false,
     operationSystem: "Windows 10 Professional Mar",
     videoCard: {
       capacity: "up to 2 GB",
@@ -238,6 +245,7 @@ export const computersData: IComputer[] = [
     },
     refurbished: true,
     operationSystem: "Fara sistem de operare",
+    hasNoOS: true,
     videoCard: {
       chip: "Intel",
       memoryFrequency: 1333,
@@ -252,5 +260,52 @@ export const computersData: IComputer[] = [
       usb_3: 4,
       network: 1
     },
+  },
+  {
+    id: "hp-prodesk-600-g4-tower-intel-core-i7-8700t-2.40-ghz-16gb-ddr4-256gb-ssd-intel-uhd-graphics-630-windows-10-pro-mar-preinstalat",
+    images: [
+      HPProdeskImg1,HPProdeskImg2, HPProdeskImg3, HPProdeskImg4, HPProdeskImg5
+    ],
+    category: "calculatoare-refurbished",
+    title:
+      "HP Prodesk 600 G4 Tower Intel Core i7-8700T 2.40 GHz, 16GB DDR4, 256GB SSD, Intel UHD Graphics 630 + Windows 10 Pro OEM",
+    price: 2106.89,
+    oldPrice: 2633.61,
+    discount: 526.72,
+    inStock: true,
+    currency: "lei",
+    details:
+      "Toate fotografiile produselor prezentate au caracter informativ, pot diferi fata de produsul vandut si pot arata accesorii ce nu sunt incluse in pachetul standard al produsului",
+    internalCode: "HPPRO600G4RE02",
+    model: "ProDesk 600 G4",
+    producerDetails: {
+      name: "HP",
+      model: "ProDesk 600 G4",
+      warranty: "12 luni",
+    },
+    processorDetails: {
+      processorModel: "i7-8700T",
+      nuclearNumber: 6,
+      processorNumber: 1,
+      processorType: "Intel Core i7",
+      producer: 'Intel',
+      cpuCache: "12",
+      frequencyCPU: "2.40",
+      frequencyTurbo: '4.00',
+      graphicProcessor: "Intel UHD Graphics 630",
+      socket: "FCLGA1151",
+    },
+    memoryCapacity: 16,
+    memoryType: "DDR4",
+    hardDiskDetails: {
+      capacity: "256 GB",
+      type: "SSD",
+    },
+    motherBoard: {
+      cpuSocket: "FCLGA1151",
+      typeMemory: 'DDR4',
+    },
+    hasNoOS:false, 
+    refurbished: true
   },
 ];
