1c3d4716d5858725df978690bad76dba91bd0224
zlatina-moga <zlatina.delcheva@gmail.com>
2023-01-07 12:30:59 +0200

    modified login and register pages

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,68 +1,63 @@
+import { useState } from "react";
 import Link from "next/link";
 import Navbar from "../components/global/Navbar";
+import Image from "next/image";
+import showPasswordIcon from "../public/svg/password-show.svg";
+import hidePasswordIcon from "../public/svg/password-hide.svg";
 
 const Register = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showSecondPassword, setShowSecondPassword] = useState<boolean>(false);
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleSecondPassword = () => {
+    setShowSecondPassword(!showSecondPassword);
+  };
+
   return (
     <>
       <Navbar />
-      <div className="col-md-4 col-md-offset-4" id="login">
-        <section id="inner-wrapper" className="login">
-          <article>
-            <form>
-              <div className="form-group">
-                <div className="input-group">
-                  <span className="input-group-addon">
-                    <i className="fa fa-user"> </i>
-                  </span>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Name"
-                  />
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="input-group">
-                  <span className="input-group-addon">
-                    <i className="fa fa-envelope"> </i>
-                  </span>
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Email Address"
-                  />
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="input-group">
-                  <span className="input-group-addon">
-                    <i className="fa fa-key"> </i>
-                  </span>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Password"
-                  />
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="input-group">
-                  <span className="input-group-addon">
-                    <i className="fa fa-key"> </i>
-                  </span>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Confirm Password"
-                  />
-                </div>
-              </div>
-              <button type="submit" className="btn btn-success btn-block">
-                Submit
-              </button>
-            </form>
-          </article>
-        </section>
+      <div className="wrapper">
+        <div className="container">
+          <h1>Register</h1>
+          <form className="form">
+          <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Username" />
+            <div  style={{display: 'flex', alignItems: 'center', position: 'relative'}}>
+              {" "}
+              <input type={showPassword ? 'text': 'password'} placeholder="Password" />
+              <Image src={showPassword ? showPasswordIcon : hidePasswordIcon} style={{height: '15px', width: '15px', position: 'absolute', right: '190px', bottom: '25px'}} onClick={togglePassword} alt='image'/>
+            </div>
+            <div  style={{display: 'flex', alignItems: 'center', position: 'relative'}}>
+              {" "}
+              <input type={showSecondPassword ? 'text': 'password'} placeholder="Repeat Password" />
+              <Image src={showSecondPassword ? showPasswordIcon : hidePasswordIcon} style={{height: '15px', width: '15px', position: 'absolute', right: '190px', bottom: '25px'}} onClick={toggleSecondPassword} alt='image'/>
+            </div>
+
+            <button
+              type="submit"
+              id="login-button"
+              style={{ marginTop: "50px" }}
+            >
+              Register
+            </button>
+          </form>
+        </div>
+        <ul className="bg-bubbles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
       </div>
     </>
   );
