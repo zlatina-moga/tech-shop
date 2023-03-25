8cd9dc7ab40660bdadf014775956860657fdbd88
zlatina-moga <zlatina.delcheva@gmail.com>
2023-03-25 15:13:39 +0200

    small ui fix

--- pages/search/[search].tsx
+++ pages/search/[search].tsx
@@ -197,6 +197,7 @@ const ProcDetail = () => {
                             style={{
                               overflow: "hidden",
                               textOverflow: "ellipsis",
+                              maxHeight: "95px",
                             }}
                           >
                             {l.name}
